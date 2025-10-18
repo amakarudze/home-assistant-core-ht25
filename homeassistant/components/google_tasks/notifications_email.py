9e2862943ff151466cf96dc513f179205c3c97a2
meenakshiAS <meenakshias1995@gmail.com>
2025-10-18 13:40:54 +0200

    Modified email message

--- homeassistant/components/google_tasks/notifications_email.py
+++ homeassistant/components/google_tasks/notifications_email.py
@@ -9,19 +9,21 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def send_email_notification(task_list, email_config):
-    """Send a daily reminder email with the given tasklist."""
+    """Sending daily task email notification remider to the user."""
 
     msg = MIMEMultipart()
     msg["From"] = email_config["sender_email"]
     msg["To"] = email_config["recipient_email"]
-    msg["Subject"] = "Daily Reminder"
-    body = "Here is your Google Tasks to-do list for today:\n" + "\n".join(
-        f"- {task}" for task in task_list
+    msg["Subject"] = "Task Reminder from Home Assistant"
+    body = (
+        "Hi,\nGentle Reminder! \n\nThis is your To-do list for today:\n"
+        + "\n".join(f"- {task}" for task in task_list)
+        + "\n\nBest Wishes, \nHome Assistant"
     )
     msg.attach(MIMEText(body, "plain"))
 
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server = smtplib.SMTP(email_config["host_name"], email_config["port"])
         server.starttls()
         server.login(email_config["sender_email"], email_config["sender_password"])
         server.sendmail(
@@ -31,8 +33,11 @@ def send_email_notification(task_list, email_config):
         )
         server.quit()
         _LOGGER.info("Email sent successfully!")
+        # print("Email sent successfully!")
+
     except Exception:
-        _LOGGER.exception("Error: %s Email not sent!!")
+        _LOGGER.exception("Error: Email not sent!!")
+        # print("email not sent!!")
 
 
 # For testing only
@@ -45,7 +50,9 @@ def send_email_notification(task_list, email_config):
 #     email_config = {
 #         "sender_email": "meenu2411as@gmail.com",
 #         "sender_password": "nchk yflh zmdh ybdz",
-#         "recipient_email": "meaa24@student.bth.se",
+#         "recipient_email": "meenu2411as@gmail.com",
+#         "port": 587,
+#         "host_name": "smtp.gmail.com",
 #     }
 
 #     task_list = ["Doctor's appointment at 3pm", "Finish HA integration"]
